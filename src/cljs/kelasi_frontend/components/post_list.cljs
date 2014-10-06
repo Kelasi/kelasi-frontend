f75d64f0f7062f977b48ff5f092038c61a699564
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2014-10-06 08:53:35 +0330

    Implemented post-list

--- src/cljs/kelasi_frontend/components/post_list.cljs
+++ src/cljs/kelasi_frontend/components/post_list.cljs
@@ -1,12 +1,17 @@
 (ns kelasi-frontend.components.post-list
   (:require [om-tools.core :as omtool :include-macros true]
-            [om-tools.dom  :as dom    :include-macros true]))
+            [om-tools.dom  :as dom    :include-macros true]
+            [om.core       :as om     :include-macros true]
+            [kelasi-frontend.components.post-box :refer (post-box)]))
 
 
 
 (omtool/defcomponentk post-list
   "List of timeline posts"
-  []
+  [[:data posts all-users]]
   (render
     [_]
-    (dom/div "")))
+    (dom/div
+      (for [post posts]
+        (om/build post-box {:post post
+                            :all-users all-users})))))
