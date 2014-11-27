cc11353ceef523090c78a8bf4fa90f66cc3a091d
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2014-11-27 12:26:48 +0330

    Fixed the routes_test.cljs to preserve the location

--- test/cljs/kelasi_frontend/stores/routes_test.cljs
+++ test/cljs/kelasi_frontend/stores/routes_test.cljs
@@ -4,7 +4,8 @@
   (:require [kelasi-frontend.stores.routes :as routes]
             [kelasi-frontend.actions :as action]
             [kelasi-frontend.state :refer (app-state)]
-            [cljs.core.async :refer (chan tap untap take!)]))
+            [cljs.core.async :refer (chan tap untap take!)]
+            [mocks.location :as loc]))
 
 
 
@@ -15,6 +16,7 @@
 
 (describe "login action"
   (before [done]
+    (loc/save)
     (tap routes/done done-ch)
 
     (action/load-user :source ::login-test
@@ -23,10 +25,11 @@
 
     (action/login :source  ::login-test
                   :user-id (:id user))
-    (take! done-ch (fn [_] (done)))
-    )
+    (take! done-ch (fn [_] (done))))
 
-  (after (untap routes/done done-ch))
+  (after
+    (untap routes/done done-ch)
+    (loc/restore))
 
   (it "should put profile_name into routes/current"
     (expect (get-in @app-state [:routes :current])
@@ -34,14 +37,17 @@
 
 (describe "show-timeline action"
   (before [done]
+    (loc/save)
     (tap routes/done done-ch)
 
     (action/show-timeline :source ::show-timeline-test
-                           :timeline-id "123")
+                          :timeline-id "123")
 
     (take! done-ch (fn [_] (done))))
 
-  (after (untap routes/done done-ch))
+  (after
+    (untap routes/done done-ch)
+    (loc/restore))
 
   (it "should put timeline_id under routes/current"
     (expect (get-in @app-state [:routes :current])
