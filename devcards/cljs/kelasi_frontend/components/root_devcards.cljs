b08cf42c6611b9dbc08d36da538e334b67c1b384
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2014-11-29 20:33:37 +0330

    Modified the root component to confirm with the new routing model

--- devcards/cljs/kelasi_frontend/components/root_devcards.cljs
+++ devcards/cljs/kelasi_frontend/components/root_devcards.cljs
@@ -1,7 +1,32 @@
 (ns kelasi-frontend.components.root-devcards
   (:require-macros [devcards.core :refer (defcard)])
   (:require [kelasi-frontend.components.root :refer (root)]
+            [kelasi-frontend.state :refer (app-state)]
+            [kelasi-frontend.components.entrance-page :refer (entrance-page)]
             [devcards.core :as dc :include-macros true]))
 
-(defcard root-component
-  (dc/om-root-card root {:test "Hello World!"}))
+(defcard global-state
+  (dc/edn-card @app-state))
+
+
+
+(defcard root-component-with-no-page
+  (dc/om-root-card root {}))
+
+
+
+(def mini-state
+  {:errors {}
+   :users {}
+   :timelines {}
+   :search {}})
+
+(def mini-state-entrance
+  (merge mini-state
+         {:routes
+          {:current-page
+           {:page entrance-page
+            :params {}}}}))
+
+(defcard root-component-with-entrance-page
+  (dc/om-root-card root mini-state-entrance))
--- src/cljs/kelasi_frontend/components/root.cljs
+++ src/cljs/kelasi_frontend/components/root.cljs
@@ -1,10 +1,15 @@
 (ns kelasi-frontend.components.root
   (:require [schema.core :as s :include-macros true]
+            [om.core :as om :include-macros true]
             [om-tools.core :as ot :include-macros true]
             [om-tools.dom :as dom :include-macros true]))
 
 
 
 (ot/defcomponentk root [data :- s/Any]
-  (render [_]
-          (dom/pre {:id "test"} (.stringify js/JSON (clj->js data) nil 2))))
+  (render
+    [_]
+    (let [page (get-in data [:routes :current-page])]
+      (if (:page page)
+        (om/build (:page page) data {:opts {:params (:params page)}})
+        (dom/div "Loading...")))))
